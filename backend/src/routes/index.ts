6d43b34668fc858c815939af3a590c6a3cc1905d
tGiech22 <tonygiech@gmail.com>
2025-09-23 22:16:12 -0400

    adding izzy's changes

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
 import { ping, dbTest, serveSwagger } from '../controllers/health.ts';
+import { getMovie } from '../controllers/tmdb.ts';
 
 const router = Router();
 
 router.get('/api/ping', ping);
 router.get('/api/db-test', dbTest);
 router.get('/swagger-output.json', serveSwagger);
+//router.get('/api/test-tmdb/:movieId', getMovie);
+router.get("/movies/:movieId", getMovie);
 
 export default router;
