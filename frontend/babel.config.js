e95ecbdb6739be93321feb3e1cfc1dd63e40260b
abby-stevenson <147872370+abby-stevenson@users.noreply.github.com>
2025-10-05 20:36:09 -0400

    Adding tailwind

---
+++ frontend/babel.config.js
@@ -0,0 +1,17 @@
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo', '@babel/preset-typescript'],
+    plugins: [
+      '@babel/plugin-transform-react-jsx',
+      'nativewind/babel',
+      [
+        'module:react-native-dotenv',
+        {
+          moduleName: '@env',
+          path: '.env',
+        },
+      ],
+    ],
+  };
+};
