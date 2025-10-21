f1ec9ddc76688e3ee2cef4fb405557bf340915f2
eric-kitagawa <kitagawa.e@northeastern.edu>
2025-10-21 01:09:58 -0400

    Add bottom nav bar

---
+++ frontend/styles/BottomNavBar.styles.ts
@@ -0,0 +1,20 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  bar: {
+    position: "absolute",
+    left: 0,
+    right: 0,
+    bottom: 0,
+    flexDirection: "row",
+    backgroundColor: "#fff",
+    borderTopWidth: 1,
+    borderTopColor: "#eee",
+    paddingBottom: 10,
+    paddingTop: 10,
+    justifyContent: "space-around",
+  },
+  item: { alignItems: "center", justifyContent: "center" },
+  activeIcon: { color: "#9A0169", fontWeight: "600" },
+  icon: { color: "#9A0169", fontWeight: "100", opacity: 0.6, },
+});
