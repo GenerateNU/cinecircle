1980ed985150af88abcdbb5f2a33abe2dbbe1d23
eric-kitagawa <kitagawa.e@northeastern.edu>
2025-11-05 00:10:06 -0500

    Change tag colors to match lo-fi

--- frontend/styles/Tag.styles.ts
+++ frontend/styles/Tag.styles.ts
@@ -6,20 +6,22 @@ export const styles = StyleSheet.create({
     container: {
         alignSelf: "flex-start",
         alignItems: "center",
+        backgroundColor: "#F4E5F0",
         borderWidth: 2,
         borderRadius: width * 0.05,
-        borderColor: '#333',
-        paddingVertical: height * 0.005,
+        borderColor: '#9A0169CC',
+        paddingVertical: height * 0.006,
         paddingHorizontal: width * 0.04,
     },
     text: {
+        color: "#9A0169",
         fontWeight: "400",
         fontSize: width * 0.04
     },
     pressed: {
         color: "#FFFFFF",
         backgroundColor: "#9A0169",
-        borderColor: "#9A0169",
+        borderColor: "#5C013F",
     },
     pressedText: {
         color: "#FFFFFF"
