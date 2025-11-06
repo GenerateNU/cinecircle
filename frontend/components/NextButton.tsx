c8127e703275a65e0d18593c5c51a90e23e0c5d6
eric-kitagawa <kitagawa.e@northeastern.edu>
2025-11-05 21:40:17 -0500

    Rework button

--- frontend/components/NextButton.tsx
+++ frontend/components/NextButton.tsx
@@ -1,20 +1,23 @@
-import { TouchableOpacity, Text, StyleSheet } from "react-native";
+import { TouchableOpacity, Text } from "react-native";
 import { styles } from "../styles/NextButton.styles"
 
 interface NextButtonProps {
+  title?: string;
   onPress: () => void;
   size?: "xs" | "small" | "medium" | "large";
+  variation?: "variation1" | "variation2";
 }
 
-
-export default function NextButton({ onPress, size = "medium" }: NextButtonProps) {
+export default function NextButton({ title = "Next", onPress, size = "medium", variation = "variation2" }: NextButtonProps) {
   return (
     <TouchableOpacity
-      style={[styles.button, styles[size]]}
+      style={[styles.button, styles[size], styles[variation]]}
       onPress={onPress}
       activeOpacity={0.7}
     >
-      <Text style={styles.text}>Next</Text>
+      <Text style={[styles.text, variation === "variation1" ? styles.textVariation1 : styles.textVariation2]}>
+        {title}
+      </Text>
     </TouchableOpacity>
   );
 }
