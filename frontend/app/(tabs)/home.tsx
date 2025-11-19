b9e7451a1bed4af6643380b56f783194e5daa6f9
eric-kitagawa <kitagawa.e@northeastern.edu>
2025-11-19 18:48:04 -0500

    Add signout button back to home screen

--- frontend/app/(tabs)/home.tsx
+++ frontend/app/(tabs)/home.tsx
@@ -1,4 +1,7 @@
 import HomeScreen from '../../screen/HomeScreen';
+import { useContext } from 'react';
+import { AuthContext } from '../../context/AuthContext';
 export default function HomeRoute() {
-  return <HomeScreen />
+    const { user, signOut } = useContext(AuthContext);
+    return <HomeScreen user={user} onSignOut={signOut}/>
 }
