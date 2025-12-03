6524a85063b0bb2d645e2af618c82a074a4f5f6d
Dylan Anctil <134968796+danctila@users.noreply.github.com>
2025-12-03 15:47:44 -0500

    Placeholder image update

--- frontend/app/events/components/EventCard.tsx
+++ frontend/app/events/components/EventCard.tsx
@@ -12,7 +12,7 @@ interface EventCardProps {
 }
 
 const PLACEHOLDER_IMAGE =
-  'https://www.bostonteapartyship.com/wp-content/uploads/2023/03/boston-fall.jpg';
+  'https://community.softr.io/uploads/db9110/original/2X/7/74e6e7e382d0ff5d7773ca9a87e6f6f8817a68a6.jpeg';
 
 export default function EventCard({
   event,
