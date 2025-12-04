59d3c38270472fcfc7fcb08cbcad5b0a42d9f99b
Alison Ryan <alisonryan@Alisons-MacBook-Air-2.local>
2025-12-04 12:24:11 -0500

    added deep links for emails

--- frontend/app.config.js
+++ frontend/app.config.js
@@ -1,9 +1,12 @@
 export default {
   expo: {
+    scheme: 'cinecircle',
+    deepLinking: true,
     extra: {
       supabaseUrl: process.env.SUPABASE_URL,
       supabaseAnonKey: process.env.SUPABASE_ANON_KEY,
       apiBaseUrl: process.env.API_BASE_URL,
+      supabaseRedirectUrl: "cinecircle://username",
       eas: {
         projectId: '6a01b6dd-ef8f-47b0-b777-9a03d0ed0453',
       },
